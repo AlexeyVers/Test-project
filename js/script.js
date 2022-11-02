b5cc276f3f22f9e05210118029d52e9f75057d77
Alex <juk50075@gmail.com>
2022-11-02 12:52:51 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.header-toggle');
+
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
